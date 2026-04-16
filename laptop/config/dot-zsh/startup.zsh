ca2c57e5f3cc5a38b2223d7b727a62796e297ff2
20_NickName_20 <20nickname20@gmail.com>
2026-04-16 15:18:58 +0300

    Update startup.zsh on laptop

--- laptop/config/dot-zsh/startup.zsh
+++ laptop/config/dot-zsh/startup.zsh
@@ -1,12 +1,22 @@
 alias nikofetch='fastfetch'
 
-fortune $HOME/.zsh/data/wm-fortunes | twm-rs -w 50 -h 13 -x 2 -y 0
-printf '\033[6A'
-{
-    todo-checker "$HOME/Notes/obsidian/vault1/"
-    echo "- - - - - - - - - - - - - -"
-    bday-check-rs "$HOME/Notes/obsidian/vault1/Birthday List.md" 20
-} | twm-rs -w auto -h auto -a left -x 6 -y 0 -t 0 --clear --color 93
+if (( $COLUMNS > 52 )); then
+    fortune $HOME/.zsh/data/wm-fortunes | twm-rs -w 45 -h 11 -x 2 -y 0
+fi
+if (( $COLUMNS > 62 )); then
+    printf '\033[4A'
+    {
+        todo-checker "$HOME/Notes/obsidian/vault1/"
+        echo "- - - - - - - - - - - - - -"
+        bday-check-rs "$HOME/Notes/obsidian/vault1/Birthday List.md" 20
+    } | twm-rs -w auto -h auto -a left -x 6 -y 0 -t 0 --clear --color 93
+elif (( $COLUMNS > 56 )); then
+    {
+        todo-checker "$HOME/Notes/obsidian/vault1/"
+        echo "- - - - - - - - - - - - - -"
+        bday-check-rs "$HOME/Notes/obsidian/vault1/Birthday List.md" 20
+    } | twm-rs -w auto -h auto -a left -x 0 -y 0 -t 0 --clear --color 93
+fi
 
 TMOUT=300
 
